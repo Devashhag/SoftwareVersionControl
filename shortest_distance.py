0c6395ba0e6c67fc91a1cd715f391dcb3a1e14c5
Devashish Agarwal <devashish.agarwal17@gmail.com>
2023-11-29 01:54:13 +0530

    updated the manhattan distance in shortest distance

--- shortest_distance.py
+++ shortest_distance.py
@@ -5,3 +5,6 @@ def heurestic_fn(a, b):
     distance = math.sqrt((a.i - b.i)**2 + (a.j - b.j)**2)
     return distance
 
+def manhattan(a,b):
+    distance=abs(a.i-b.i)+abs(a.j-b.j)
+    return distance
