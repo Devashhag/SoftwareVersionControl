10af821a97c59ae10dea6f1e007ac9046f8397f8
Devashish Agarwal <devashish.agarwal17@gmail.com>
2023-11-29 01:31:16 +0530

    adding new features in feature 1 branch

---
+++ colors.py
@@ -0,0 +1,12 @@
+#####
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+grey = (255,255,255)
+white = (255, 255, 255)
+turq = (64, 224, 208)
+yellow = (255, 195, 0)
+lightgr = (12, 244, 174)
+lightyellow = (230, 244, 12)
+#####
+#initialization of colors
--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+##final  code to implement A* star on any device
+'''
+ start -->       9,65
+ fb->9,48
+second bloc-->     23,48
+third block->      27,48
+fourth block->      21,56
+fifth->          31,56
+design 6 ->      19,39
+deisgn 7 ->      23,39
+deisgn 8-> 
+ninth->        35,42
+tenth->       48,41
+eleventh->     57,43
+hubble->9,60
+lib->       14,28
+it ->26,28
+barons->6,32
+mac->6,40
+food coure->30,59
+ground->23,14
+amphi  ->34,49
+md guest house->10,30
+frisco->26,51
+gandhi chowk->    34,38
+tulips=>50,37
+boys ->53,35
+lab->55,35
+'''
+
+
 from libraries import *
+from colors import *
+from shortest_distance import *
